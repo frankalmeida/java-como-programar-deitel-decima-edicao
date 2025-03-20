3fc825c4475ae4b0d3a1683555704030468231d0
Frank Almeida <frank@haf.com.br>
2025-03-19 21:45:17 -0300

    feat: add atividades do livro até secao 2.5.x

---
+++ javacomoprogramardeiteldecimaedicao/src/com/deitel/capitulo2/Welcome2.java
@@ -0,0 +1,13 @@
+package com.deitel.capitulo2;
+
+public class Welcome2 {
+	public static void main(String[] args) {
+		System.out.print("Welcome to ");
+		System.out.println("Java Programming!");
+		System.out.println("Welcome\nto\nJava\nProgramming!");
+		System.out.println("\"entre aspas\"");
+		System.out.printf("%s%n%s%n",
+				"Welcome to", "Java Programming!");
+	}
+
+}
