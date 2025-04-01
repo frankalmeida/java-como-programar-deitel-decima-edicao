2b357a5f734041d678c84dee4bc6d98834556d3f
Frank Acer <frankalmeida7@gmail.com>
2025-03-31 21:27:02 -0300

    feat: conclui capitulo 4

--- javacomoprogramardeiteldecimaedicao/src/com/deitel/capitulo4/ClassAverage.java
+++ javacomoprogramardeiteldecimaedicao/src/com/deitel/capitulo4/ClassAverage.java
@@ -8,19 +8,27 @@ public class ClassAverage {
 		Scanner input = new Scanner(System.in);
 
 		int total = 0;
-		int gradeCounter = 1;
+		int gradeCounter = 0;
 
-		while (gradeCounter <= 10) {
-			System.out.println("Enter grade: ");
-			int grade = input.nextInt();
+		System.out.print("Enter grade or -1 to quit: ");
+		int grade = input.nextInt();
+
+		while (grade != -1) {
 			total = total + grade;
 			gradeCounter = gradeCounter + 1;
+
+			System.out.print("Enter grade or -1 to quit: ");
+			grade = input.nextInt();
 		}
 
-		int average = total / 10;
+		if (gradeCounter != 0) {
+			double average = (double) total / gradeCounter;
 
-		System.out.printf("%nTotal of all 10 grades is %d%n", total);
-		System.out.printf("Class average is %d%n", average);
+			System.out.printf("%nTotal of the %d grades entered is %d%n", gradeCounter, total);
+			System.out.printf("Class average is %.2f%n", average);
+		} else {
+			System.out.println("No grades were entered");
+		}
 	}
 
 }
