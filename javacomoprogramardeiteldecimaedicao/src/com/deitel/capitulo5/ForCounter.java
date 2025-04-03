99a5a381d99ccdb82dd2260c3ea94425df02ce2e
Frank Acer <frankalmeida7@gmail.com>
2025-04-02 21:30:24 -0300

    feat: inicio do ca 5 e atividades até secao 5.6

---
+++ javacomoprogramardeiteldecimaedicao/src/com/deitel/capitulo5/ForCounter.java
@@ -0,0 +1,14 @@
+package com.deitel.capitulo5;
+
+public class ForCounter {
+	public static void main(String[] args) {
+
+		for (int counter = 1; counter <= 10; counter++) {
+			System.out.printf("%d ", counter);
+
+		}
+
+		System.out.println();
+	}
+
+}
---
+++ javacomoprogramardeiteldecimaedicao/src/com/deitel/capitulo5/WhileCounter.java
@@ -0,0 +1,14 @@
+package com.deitel.capitulo5;
+
+public class WhileCounter {
+	public static void main(String[] args) {
+		int counter = 0;
+		while (++counter <= 10) {
+			System.out.printf("%d ", counter);
+
+		}
+
+		System.out.println();
+	}
+
+}
