e49ad8af9cea10b8609c39a3ce69636b7aca3333
Frank Acer <frankalmeida7@gmail.com>
2025-04-23 21:59:50 -0300

    feat: atividades até o topico 7.9

---
+++ javacomoprogramardeiteldecimaedicao/src/com/deitel/capitulo7/EnhancedForTest.java
@@ -0,0 +1,16 @@
+package com.deitel.capitulo7;
+
+import java.util.Iterator;
+
+public class EnhancedForTest {
+    public static void main(String[] args) {
+	int[] array = { 87, 68, 94, 100, 83, 78, 85, 91, 76, 87 };
+	int total = 0;
+
+	for (int number : array) {
+	    total += number;
+	}
+
+	System.out.printf("Total of array elements: %d%n", total);
+    }
+}
