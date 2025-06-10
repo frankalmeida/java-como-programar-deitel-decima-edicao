ca87b973890316936a5932ec549c7822218611d0
Frank Acer <frankalmeida7@gmail.com>
2025-06-09 21:46:14 -0300

    feat: finaliza atividades do capitulo 15

---
+++ javacomoprogramardeiteldecimaedicao/src/com/deitel/capitulo15/ReadSequentialFile.java
@@ -0,0 +1,59 @@
+package com.deitel.capitulo15;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ReadSequentialFile {
+    private static ObjectInputStream input;
+
+    public static void main(String[] args) {
+	openFile();
+	readRecords();
+	closeFile();
+    }
+
+    public static void openFile() {
+	try // abre o arquivo
+	{
+	    input = new ObjectInputStream(Files.newInputStream(Paths.get("clients.ser")));
+	} catch (IOException ioException) {
+	    System.err.println("Error opening file.");
+	    System.exit(1);
+	}
+    }
+
+    public static void readRecords() {
+	System.out.printf("%-10s%-12s%-12s%10s%n", "Account", "First Name", "Last Name", "Balance");
+
+	try {
+	    while (true) // faz um loop até ocorrer uma EOFException
+	    {
+		Account record = (Account) input.readObject();
+
+		// exibe o conteúdo de registro continua526 Java: como programar
+		System.out.printf("%-10d%-12s%-12s%10.2f%n", record.getAccount(), record.getFirstName(), record.getLastName(), record.getBalance());
+	    }
+	} catch (EOFException endOfFileException) {
+	    System.out.printf("%No more records%n");
+	} catch (ClassNotFoundException classNotFoundException) {
+	    System.err.println("Invalid object type. Terminating.");
+	} catch (IOException ioException) {
+	    System.err.println("Error reading from file. Terminating.");
+	}
+    }
+
+    // fecha o arquivo e termina o aplicativo
+    public static void closeFile() {
+	try {
+	    if (input != null)
+		input.close();
+	} catch (IOException ioException) {
+	    System.err.println("Error closing file. Terminating.");
+	    System.exit(1);
+	}
+    }
+
+}
